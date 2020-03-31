024001f22e7b77ef63546cb25038cf83971db0f6
yurachistic1 <yurachistic@gmail.com>
2020-03-31 13:14:04 +0100

    login styling

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,25 +1,61 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="person"
+                    type="text"
+                  />
 
-    <input 
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input 
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-    <br>
-    <button
-      @click="login">
-      Login
-    </button>
-  </div>
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn 
+                  color="primary"
+                  @click="login">
+                  Login
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
+
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
