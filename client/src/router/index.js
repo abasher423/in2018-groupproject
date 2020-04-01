062f0bd472622196000840743f9c7257678b9072
yurachistic1 <yurachistic@gmail.com>
2020-04-01 03:10:37 +0100

    removed route to template

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Signup from '@/components/Login'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -9,13 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/login',
       name: 'login',
-      component: Signup
+      component: Login
     }
   ]
 })
