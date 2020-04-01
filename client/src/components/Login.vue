47977af749f5cd3a6f76048f88c065168f2410b4
yurachistic1 <yurachistic@gmail.com>
2020-04-01 04:06:03 +0100

    added user and login flag states

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -79,10 +79,11 @@ export default {
           uniqueNumber: this.uniqueNumber,
           password: this.password
         })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
       } catch (error){
         this.error = error.response.data.error
       }
-       console.log(response.data)
     }
   }
 }
