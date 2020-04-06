22c6ae273861846632bdac3e8f99756a5872bb92
yurachistic1 <yurachistic@gmail.com>
2020-04-06 21:27:30 +0100

    Added frontend transaction component

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Menu from '@/components/Menu'
+import Transactions from '@/components/Transactions'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/menu',
       name: 'menu',
       component: Menu
+    },
+    {
+      path: '/transactions',
+      name: 'transactions',
+      component: Transactions
     }
   ]
 })
