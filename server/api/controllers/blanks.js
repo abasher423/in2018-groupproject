dd394b2aa9fb9e6c3be1c74fbaf00e88dad6efa2
Yury Chistov <yurachistic@gmail.com>
2020-04-16 16:42:01 +0100

    better error messages

--- server/api/controllers/blanks.js
+++ server/api/controllers/blanks.js
@@ -75,7 +75,7 @@ exports.blanks_get_blank = (req, res, next) => {
         } else {
           res
             .status(404)
-            .json({ message: "No valid entry found for provided ID" });
+            .json({ message: `No valid entry found for provided number` });
         }
       })
       .catch(err => {
@@ -99,7 +99,7 @@ exports.blanks_get_blank_by_uniqueNo = (req, res, next) => {
       } else {
         res
           .status(404)
-          .json({ message: "No valid entry found for provided number" });
+          .json({ message: `No valid entry found for provided number ${req.params.blankId}` });
       }
     })
     .catch(err => {
@@ -129,11 +129,17 @@ exports.blanks_update_blank = (req, res, next) => {
     });
 }
 exports.blanks_delete_blank = (req, res, next) => {
-    const id = req.params.blankId;
-    Blank.remove({ _id: id })
-    .exec()
-    .then(result => {
-      res.status(200).json({
+  const id = req.params.blankId;
+  Blank.findOne({ _id: id })
+  .exec()
+  .then(blank => {
+    if(blank.used){
+      return res.status(500).json({
+        message: `Cannot remove used blank: ${blank.uniqueNumber}`
+      })
+    } else {
+      Blank.remove({_id: blank._id}).exec().then(result => {
+        res.status(200).json({
         message: "Blank deleted"
       });
     })
@@ -142,16 +148,38 @@ exports.blanks_delete_blank = (req, res, next) => {
       res.status(500).json({
         error: err
       });
+    });  
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
     });
+  });
 }
 exports.blanks_delete_blank_by_number = (req, res, next) => {
   const un = req.params.uniqueNumber
-  Blank.deleteOne({ uniqueNumber: un })
+  Blank.findOne({ uniqueNumber: un })
   .exec()
-  .then(result => {
-    res.status(200).json({
-      message: "Blank deleted"
-    });
+  .then(blank => {
+    if(blank.used){
+      return res.status(500).json({
+        message: `Cannot remove used blank: ${blank.uniqueNumber}`
+      })
+    } else {
+      Blank.remove({_id: blank._id}).exec().then(result => {
+        res.status(200).json({
+        message: "Blank deleted"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });  
+    }
   })
   .catch(err => {
     console.log(err);
