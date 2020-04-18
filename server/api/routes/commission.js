a1aa0017c5f1bd4300f4c35042de1828f2198dd0
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2020-04-18 05:07:29 +0100

    added router to write to json file

--- server/api/routes/commission.js
+++ server/api/routes/commission.js
@@ -16,4 +16,29 @@ router.get("/", function(req, res){
     }
 }) 
 
+router.post("/", function(req, res){
+    try{
+        const fileName = path.resolve("server",'../commission.json');
+        const file = require(fileName);
+        
+        file.commission.interlines = req.body.interlines;
+        file.commission.domestic = req.body.domestic;
+
+        fs.writeFile(fileName, JSON.stringify(file, null, 2), function(err){
+            if (err){
+                return console.log(err);
+            }
+            console.log("the file was saved");
+            res.status(200).json({
+                response: file
+            });
+        });
+    } catch(err){
+        res.status(500).json({
+            message: "Error writing to file",
+            error: err
+        });
+    }
+})
+
 module.exports = router;
